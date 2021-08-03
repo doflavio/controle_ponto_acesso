89ddc4eff511bd2cb34ba19b2b4d6a90d009fe99
Flávio Leandro Pereira <doflavio@gmail.com>
2021-08-03 14:31:35 -0300

    Classes de modelo

---
+++ src/main/java/io/github/doflavio/controle_ponto_acesso/model/Ocorrencia.java
@@ -0,0 +1,20 @@
+package io.github.doflavio.controle_ponto_acesso.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class Ocorrencia {
+    @Id
+    private long id;
+    private String nome;
+    private String descricao;
+}
---
+++ src/main/java/io/github/doflavio/controle_ponto_acesso/model/TipoData.java
@@ -0,0 +1,19 @@
+package io.github.doflavio.controle_ponto_acesso.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@Entity
+public class TipoData {
+    @Id
+    private long id;
+    private String descricao;
+}
