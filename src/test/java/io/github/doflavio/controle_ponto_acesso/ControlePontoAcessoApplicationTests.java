d260c9cf543d0f140980f0f4e7d15977c332e136
Flávio Leandro Pereira <doflavio@gmail.com>
2021-08-03 09:44:29 -0300

    Criação do projeto

---
+++ src/test/java/io/github/doflavio/controle_ponto_acesso/ControlePontoAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.doflavio.controle_ponto_acesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePontoAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
